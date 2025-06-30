1bb327083a69326c1a4025f2aad4fca448d7c62f
sankalp verma <vsankalp099@gmail.com>
2025-07-01 00:19:34 +0530

    backendpro

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+require('dotenv').config();
+
+const port = process.env.PORT || 8000
+
+const myapp = express();
+      myapp.get("/",(req,res)=>{
+        res.send("welcome to express js");
+      })
+
+      myapp.get("/about ",(req,res)=>{
+        res.send("this is a about pageeeeeeeeeeeeeeeee")
+      })
+
+myapp.listen(port,()=>{
+    console.log(`server is runing:${port}`)
+})      
+
+
